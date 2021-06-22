552954c46d2b690efe9a78a2470e289aec5e31a5
Ricardo Tavares <ricardotavares51@hotmail.com>
2021-06-22 11:27:27 -0300

    Criação de uma função que adiciona uma nova mensagem no chat

--- database/scripts/crud.py
+++ database/scripts/crud.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from .connect import Connect
 
 class CRUD(object) :
@@ -13,11 +14,13 @@ class CRUD(object) :
         resp = self.database.cursor.execute('SELECT * FROM Mensagem WHERE Mensagem.idTrabalho = ? ORDER BY Mensagem.dia', (idTrabalho,))
         return resp.fetchall()
 
-    def imprimirTrabalhosPorID(self, id: int):
-        lista = self.getTrabalhos(idUsuario= id)
-        print('{:>3s} {:>3s} {:>3s} {:>3s} {:21s} {:>3s}'.format(
-            'id', 'idUsuario', 'idPrefeitura', 'idRua', 'titulo', 'status'))
-        for c in lista:
-            print('{:3d} {:3d} {:3d} {:3d} {:s} {:d}'.format(
-                c[0], c[1], c[2],
-                c[3], c[4], c[5]))
+    def insertMensagemTrabalho(self, idTrabalho: int, idSender: int, idReceive: int, msg: str):
+        # current date and time
+        now = datetime.now()
+        s2 = now.strftime("%Y-%m-%d %H:%M:%S")
+        resp = self.database.cursor.execute(
+            'INSERT INTO Mensagem (idTrabalho, idSender, idReceive, msg, dia) VALUES(?,?,?,?,?)',
+            (idTrabalho, idSender, idReceive, msg, s2))
+        self.database.commit_db()
+        return resp.fetchone()
+        
